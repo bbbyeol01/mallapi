e4b231a2e799058487ce9f7a8af90426bcefdbc4
bbbyeol <forbyeol@gmail.com>
2024-07-29 22:38:11 +0900

    Controller
    - Todo 등록, 조회, 수정, 삭제를 위한 RestController 생성
    - DataFomatter 생성
    - 올바르지 않은 인자(존재하지 않는 tno 또는 영문)으로 조회 요청(get) 했을 때 white label page가 아닌 오류 메시지를 반환할 수 있도록 함

--- src/main/java/com/zerock/mallapi/contoller/TodoController.java
+++ src/main/java/com/zerock/mallapi/contoller/TodoController.java
@@ -7,10 +7,9 @@ import com.zerock.mallapi.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -30,6 +29,35 @@ public class TodoController {
         log.info("pageRequestDTO, {}", pageRequestDTO);
 
         return todoService.list(pageRequestDTO);
+    }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+        log.info("TodoDTO : {}", todoDTO);
+
+        Long tno = todoService.register(todoDTO);
+
+        return Map.of("TNO", tno);
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable(name = "tno") Long tno, @RequestBody TodoDTO todoDTO) {
+        todoDTO.setTno(tno);
+
+        log.info("Modify : {}", todoDTO);
+        todoService.modify(todoDTO);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+
+        log.info("Remove : {}", tno);
+
+        todoService.remove(tno);
+
+        return Map.of("RESULT", "SUCCESS");
     }
 }
 
